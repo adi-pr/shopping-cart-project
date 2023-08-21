99ac708f804194d8170062605ca6812404d52129
Ruben <adityaprasad7123@gmail.com>
2023-08-21 16:16:20 -0400

    Added Context for cart

--- src/pages/Store/Store.jsx
+++ src/pages/Store/Store.jsx
@@ -1,15 +1,20 @@
+import React, { useState } from 'react';
 import Sidebar from "./Sidebar";
 import { Outlet } from "react-router-dom";
 import './index.css'
+import { CartProvider } from '../../utils/CartContext';
 
 const StorePage = () => {
+
     return (
-        <div id="store" className="mt-32 grid ">
-            <Sidebar />
-            <div className="pt-8 pr-8 mb-8">
-                <Outlet />
+        <CartProvider>
+            <div id="store" className="mt-32 grid ">
+                <Sidebar />
+                <div className="pt-8 pr-8 mb-8">
+                    <Outlet />
+                </div>
             </div>
-        </div>
+        </CartProvider>
     );
 }
 
