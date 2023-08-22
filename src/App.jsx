0895726874eade0f43a2725b53c116990ed95631
Ruben <adityaprasad7123@gmail.com>
2023-08-22 15:29:28 -0400

    Updated CartProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import Navbar from "./components/Navbar"
 import RoutesConfig from './routes/routes';
 import { CartProvider } from './utils/CartContext';
--- src/pages/Store/Store.jsx
+++ src/pages/Store/Store.jsx
@@ -2,19 +2,15 @@ import React, { useState } from 'react';
 import Sidebar from "./Sidebar";
 import { Outlet } from "react-router-dom";
 import './index.css'
-import { CartProvider } from '../../utils/CartContext';
 
 const StorePage = () => {
-
     return (
-        <CartProvider>
             <div id="store" className="mt-32 grid ">
                 <Sidebar />
                 <div className="pt-8 pr-8 mb-8 flex flex-wrap justify-between gap-5">
                     <Outlet />
                 </div>
             </div>
-        </CartProvider>
     );
 }
 
