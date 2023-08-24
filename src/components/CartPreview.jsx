b95f8d2ee8042495b909cffcfed27ad3d87ae668
Ruben <adityaprasad7123@gmail.com>
2023-08-24 10:38:55 -0400

    Added overflow-y hidden when cart is open

--- src/components/CartPreview.jsx
+++ src/components/CartPreview.jsx
@@ -1,20 +1,28 @@
+import { useEffect } from 'react';
 import { useCart } from "../utils/CartContext";
 
 const CartPreview = ({ toggleShowCart }) => {
     const { cart } = useCart()
 
+    useEffect(() => {
+        document.body.style.overflowY = 'hidden'
+        
+        return () => {
+            document.body.style.overflowY = 'auto'
+        };
+    }, []);
 
 
-    return ( 
-        <section className="border bg-neutral-900 absolute top-0 right-0 p-5 z-50 h-full">
-            <div>
+    return (
+        <section className="border bg-neutral-900 absolute top-0 right-0 p-5 z-60 h-full overflow-x-hidden">
+            <div className="p-10">
                 Hello
                 <button onClick={toggleShowCart}>
                     Close
                 </button>
             </div>
         </section>
-     );
+    );
 }
- 
-export default CartPreview;
+
+export default CartPreview; 
