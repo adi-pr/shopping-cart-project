c19e3f457f8aa4d536ba7dfa1f677ecceca6d6c9
Ruben <adityaprasad7123@gmail.com>
2023-08-24 12:49:40 -0400

    Added prop validation and render cart item names to page

--- src/components/CartPreview.jsx
+++ src/components/CartPreview.jsx
@@ -1,12 +1,13 @@
 import { useEffect } from 'react';
 import { useCart } from "../utils/CartContext";
+import PropTypes from 'prop-types'
 
 const CartPreview = ({ toggleShowCart }) => {
     const { cart } = useCart()
 
     useEffect(() => {
         document.body.style.overflowY = 'hidden'
-        
+
         return () => {
             document.body.style.overflowY = 'auto'
         };
@@ -16,7 +17,7 @@ const CartPreview = ({ toggleShowCart }) => {
     return (
         <section className="border bg-neutral-900 absolute top-0 right-0 p-5 z-60 h-full overflow-x-hidden">
             <div className="p-10">
-                Hello
+                <ItemList cart={cart} />
                 <button onClick={toggleShowCart}>
                     Close
                 </button>
@@ -25,4 +26,47 @@ const CartPreview = ({ toggleShowCart }) => {
     );
 }
 
-export default CartPreview; 
+export default CartPreview;
+
+
+const ItemList = ({ cart }) => {
+
+    return (
+        <>
+            {
+                cart.items.map(item => (
+                    <Item 
+                        key={item.id}
+                        brand={item.brand}
+                        model={item.model}
+                        price={item.price}
+                        imgURL={item.imgURL}
+                    />
+                ))
+            }
+        </>
+    )
+}
+
+const Item = ({ brand, model, price, imgURL }) => {
+    return ( 
+        <div>
+            {model}
+        </div>
+     );
+}
+ 
+CartPreview.propTypes = {
+    toggleShowCart: PropTypes.func.isRequired
+}
+
+ItemList.propTypes = {
+    cart: PropTypes.object.isRequired
+}
+
+Item.propTypes = {
+    brand: PropTypes.string.isRequired,
+    model: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    imgURL: PropTypes.string.isRequired
+}
