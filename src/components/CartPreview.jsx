a1e34e5c9c6bf02b2731c99ca2f9dedbc3221377
Ruben <adityaprasad7123@gmail.com>
2023-08-25 13:28:19 -0400

    Added styling and rendered cart data

--- src/components/CartPreview.jsx
+++ src/components/CartPreview.jsx
@@ -37,15 +37,14 @@ const CartPreview = ({ toggleShowCart }) => {
 
     return (
         <>
-            <div className='fixed top-0 left-0 w-full h-full bg-black opacity-50 flex justify-center items-center z-60'></div>
+            <div className='fixed top-0 left-0 w-full h-full bg-black opacity-50 flex justify-center items-center cursor-pointer z-60' onClick={toggleShowCart}/>
             <m.section
-                className="border bg-neutral-900 fixed top-0 right-0 p-5 z-60 h-full overflow-x-hidden"
+                className="rounded-xl bg-neutral-900 fixed top-0 right-0 p-5 z-60 h-full overflow-x-hidden"
                 variants={container}
                 initial='initial'
                 animate='animate'
             >
                 <m.div
-                    className="p-10"
                     variants={item}
                 >
                     <ItemList cart={cart} />
@@ -64,7 +63,7 @@ export default CartPreview;
 const ItemList = ({ cart }) => {
 
     return (
-        <>
+        <div className='flex flex-col gap-3'>
             {
                 cart.items.map(item => (
                     <Item
@@ -76,14 +75,40 @@ const ItemList = ({ cart }) => {
                     />
                 ))
             }
-        </>
+        </div>
     )
 }
 
 const Item = ({ brand, model, price, imgURL }) => {
     return (
-        <div>
-            {model}
+        <div
+            className='bg-gray-700 p-5 rounded-lg'
+        >
+            <div
+                className='relative flex gap-5'
+            >
+                <div className='p-2 rounded-lg bg-white'>
+                    <img
+                        className='h-w-28 w-28'
+                        src={imgURL}
+                        alt={model}
+                        loading='lazy'
+                    />
+                </div>
+                <div className='flex flex-col gap-1'>
+                    <p>
+                        {brand}
+                    </p>
+                    <p>
+                        {model}
+                    </p>
+                </div>
+                <div className='absolute bottom-0 right-0'>
+                    <p>
+                        {price}
+                    </p>
+                </div>
+            </div>
         </div>
     );
 }
